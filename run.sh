d8bc2c42eb4c779a8bc3e0a250664d9d8c996303
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2022-02-18 23:18:10 +0100

    Remove run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source .env
-
-#ansible-playbook -v linode-create-gpu-server.yml \
-#  --extra-vars="username=user password=linodeGPUTesting root_password=utz6mnSofqKdc52F"
-
-ansible-playbook -v linode-create-gpu-server.yml
